86434d098910a8d8dca79a9f2e6119db15d8cde0
karur4n <karuran.f@gmail.com>
2014-11-20 00:32:08 +0900

    とりあえず動いた

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,2 +1,15 @@
 class PostsController < ApplicationController
+  def index
+    @posts = Post.all
+  end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    post = params.require(:post).permit(:title, :author)
+    Post.create(post)
+    redirect_to root_path
+  end
 end
